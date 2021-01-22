6181a2bfccf946abcb9b13e088025ebba4c32ff4
pradippatil01 <mr.pradip01@gmail.com>
2021-01-22 13:52:48 +0530

    Start : Js file created and welcome message display

---
+++ UserRegistration.js
@@ -0,0 +1,11 @@
+/**************************************************************************
+*   Excecution : 1. default Node 
+*   Purpose    : Userregistration using Javascript
+*   @description 
+*   @author    : Pradip R patil (BridgeLabz)
+*   @file      : userRegistration.js
+*   @version   : v15.6.0
+***************************************************************************/
+
+console.log('Welcome to User Registration Program');
+
