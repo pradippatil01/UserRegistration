cd20630bb27c06e9023b84cde73780c6313d9788
pradippatil01 <mr.pradip01@gmail.com>
2021-01-22 13:57:22 +0530

    Add : isValidFirstName method added to valid firstname

--- UserRegistration.js
+++ UserRegistration.js
@@ -7,5 +7,24 @@
 *   @version   : v15.6.0
 ***************************************************************************/
 
-console.log('Welcome to User Registration Program');
+/**
+ * @param {firstName} holds input provided by user for firstName;
+ */
+const prompt = require('prompt-sync')();
 
+/**
+ * isValidFirstName valid user firstName
+ */
+isValidFirstName = (fistName) => {
+    const NAME_REGEX = new RegExp(`^[A-Z]{1}[a-zA-Z]{2,}$`);
+    if (NAME_REGEX.test(fistName)) {
+        console.log('firstname is valid ');
+    }
+    else {
+        console.log('firstname is not valid');
+    }
+}
+
+console.log('Welcome to User Registration Program');
+const firstName = prompt('enter firstName :- ');
+isValidFirstName(firstName);
